230afaeb205a047dd7edf03c6d3a41c7891b14e2
Tiago Sales <tiagosales@outlook.com>
2025-12-04 16:24:32 -0300

    initial commit

---
+++ src/main/java/br/com/foradarodada/foradarodadaweb/ForadarodadawebApplication.java
@@ -0,0 +1,13 @@
+package br.com.foradarodada.foradarodadaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForadarodadawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForadarodadawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/foradarodada/foradarodadaweb/ForadarodadawebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foradarodada.foradarodadaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForadarodadawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
